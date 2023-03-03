162c19012f0ad2d6515ec88c9fd9dad08fe7b466
chaselee412 <chaselee412@gmail.com>
2023-03-03 22:31:19 +0800

    yes true

---
+++ src/com/caiyu/UDPtest/TalkStudent.java
@@ -0,0 +1,8 @@
+package com.caiyu.UDPtest;
+
+public class TalkStudent {
+    public static void main(String[] args) {
+        new Thread(new TalkSend(7777,"localhost",9999)).start();
+        new Thread(new TalkReceive(8888,"teacher")).start();
+    }
+}
